2b719267225f9ff71310de7b314a786d6d0f87a7
Felix Geisendörfer <felix@debuggable.com>
2014-04-24 14:17:45 -0600

    Basic command parsing

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(0)
+	}
+	switch cmd := os.Args[1]; cmd {
+	case "init":
+		fmt.Printf("NOT DONE YET\n")
+	default:
+		fatal("Unknown command: %s", cmd)
+	}
+}
+
+func usage() {
+	fmt.Printf("browserflood <command>\n")
+	fmt.Printf("\n")
+	fmt.Printf("commands:\n")
+	fmt.Printf("  init  Initializes a browserflood project in the current directory.\n")
+}
+
+func fatal(reason string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, "error: "+reason+"\n", args...)
+	os.Exit(1)
+}
